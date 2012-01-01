a301bf8d1984af723a853b1950dde37fa735d7e0
Robert Ellison <rob@catfood.net>
2012-01-01 15:19:25 -0800

    Work Item #10187 (64bit support via the ACE driver)

--- Catfood.Shapefile/Header.cs
+++ Catfood.Shapefile/Header.cs
@@ -1,5 +1,5 @@
 /* ------------------------------------------------------------------------
- * (c)copyright 2009-2011 Catfood Software - http://catfood.net
+ * (c)copyright 2009-2012 Catfood Software and contributors - http://catfood.net
  * Provided under the ms-PL license, see LICENSE.txt
  * ------------------------------------------------------------------------ */
 
